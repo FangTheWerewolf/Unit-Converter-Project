bda191f5121eed644efc1010a76810b2a6bb9385
FangTheWerewolf <cfls3264@gmail.com>
2024-11-14 16:08:37 -0500

    Update

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const inputValue = 10;
-const milesToKm = true;
+const milesToKm = false;
 let result = 0;
 if (milesToKm) {
-result = inputValue +1.60934;
-} else { result = inputValue / 1.60934
+  result = inputValue * 1.60934;
+} else {
+  result = inputValue / 1.60934;
 }
-const resultString = inputValue + " miles are " + result + " km";
-console.log(resultString)
+console.log(milesToKm);
+
+const resultString = inputValue + "miles are " + result + " km";
+console.log(resultString);
+
+const resultElement = document.getElementById("resultElement");
