8054bdb2b8ea0d57e88d91d9aa7e2e290cf11f93
FangTheWerewolf <cfls3264@gmail.com>
2024-11-14 16:10:58 -0500

    Final Code
    Updated final code of the project, allowing for simple unit conversion.

--- script.js
+++ script.js
@@ -12,3 +12,4 @@ const resultString = inputValue + "miles are " + result + " km";
 console.log(resultString);
 
 const resultElement = document.getElementById("resultElement");
+resultElement.innerHTML = resultString;
